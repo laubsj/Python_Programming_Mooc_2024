adc97af635ef58a8cef8e359f4f5743828598425
Johann <johannlaubscher@hotmail.com>
2024-06-14 12:08:48 +1000

    Add files via upload

---
+++ Extra_Space.py
@@ -0,0 +1,30 @@
+name = "Tim Tester"
+age = 20
+skill1 = "python"
+level1 = "beginner"
+skill2 = "java"
+level2 = "veteran"
+skill3 = "programming"
+level3 = "semiprofessional"
+lower = 2000
+upper = 3000
+ 
+print(f"my name is {name}, I am {age} years old")
+print("")
+print("my skills are")
+print(" -", skill1, "("+level1+")")
+print(" -", skill2, "("+level2+")")
+print(" -", skill3, "("+level3+")")
+print("")
+print(f"I am looking for a job with a salary of {lower}-{upper} euros per month")
+
+
+
+# my name is Tim Tester, I am 20 years old
+#
+# my skills are
+#  - python (beginner)
+#  - java (veteran)
+#  - programming (semiprofessional)
+#
+# I am looking for a job with a salary of 2000-3000 euros per month
