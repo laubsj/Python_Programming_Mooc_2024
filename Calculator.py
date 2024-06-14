856a4607905faf58e806a65914e0aba0d114be4e
laubsj <johndoe@example.comjlaubscher75@gmail.com>
2024-06-14 15:57:04 +1000

    Init

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+operation = input("Operation: ")
+ 
+if operation == "add":
+    print(f"{number1} + {number2} = {number1 + number2}")
+if operation == "subtract":
+    print(f"{number1} - {number2} = {number1 - number2}")
+if operation == "multiply":
+    print(f"{number1} * {number2} = {number1 * number2}")
+
+# Number 1: 10
+# Number 2: 17
+# Operation: add
+
+# 10 + 17 = 27
+
+#Number 1: 4
+# Number 2: 6
+# Operation: multiply
+
+# 4 * 6 = 24
+
+#Number 1: 4
+#Number 2: 6
+#Operation: subtract
+
+#4 - 6 = -2
