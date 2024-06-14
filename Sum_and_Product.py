2f1667b6e93792cd6b65501db0b0ff8b42ab4abc
laubsj <johndoe@example.comjlaubscher75@gmail.com>
2024-06-14 13:33:18 +1000

    Init

---
+++ Sum_and_Product.py
@@ -0,0 +1,11 @@
+number1 = int(input('Number 1: '))
+number2 = int(input('Number 2: '))
+print("The sum of the numbers:", number1 + number2)
+print("The product of the numbers:", number1 * number2)
+
+
+
+# Number 1: 3
+# Number 2: 7
+# The sum of the numbers: 10
+# The product of the numbers: 21
