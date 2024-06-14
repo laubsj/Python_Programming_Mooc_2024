7096761d33dc06fdbb31600ffd3dfddc55eea090
laubsj <johndoe@example.comjlaubscher75@gmail.com>
2024-06-14 15:44:28 +1000

    Init

---
+++ Order_of_Magnitude.py
@@ -0,0 +1,31 @@
+
+number = int(input("Please type in a number: "))
+if number < 1000:
+    print("This number is smaller than 1000")
+ 
+if number < 100:
+    print("This number is smaller than 100")
+ 
+if number < 10:
+    print("This number is smaller than 10")
+ 
+print("Thank you!")
+
+# Please type in a number: 950
+# This number is smaller than 1000
+# Thank you!
+
+# Please type in a number: 59
+# This number is smaller than 1000
+# This number is smaller than 100
+# Thank you!
+
+# Please type in a number: 2
+# This number is smaller than 1000
+# This number is smaller than 100
+# This number is smaller than 10
+# Thank you!
+
+# Please type in a number: 1123
+# Thank you!
+
