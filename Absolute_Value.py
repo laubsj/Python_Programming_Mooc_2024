b14589a82044388290e58b50905f7a411ee6d951
laubsj <johndoe@example.comjlaubscher75@gmail.com>
2024-06-14 15:19:59 +1000

    Init

---
+++ Absolute_Value.py
@@ -0,0 +1,10 @@
+number = int(input("Please type in a number: "))
+if number < 0:
+    number *= -1
+print("The absolute value of this number is", number)
+
+# Please type in a number: -99
+# The absolute value of this number is 99
+
+# Please type in a number: 1
+# The absolute value of this number is 1s
