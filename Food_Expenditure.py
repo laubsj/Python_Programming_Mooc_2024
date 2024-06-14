4699168495d3a7a8ee1b2d99e3346a0948417b56
laubsj <johndoe@example.comjlaubscher75@gmail.com>
2024-06-14 13:47:39 +1000

    Init

---
+++ Food_Expenditure.py
@@ -0,0 +1,18 @@
+times_in_cafeteria = int(input("How many times a week do you eat at the student cafeteria? "))
+price_of_lunch = float(input("The price of a typical student lunch? "))
+groceries = float(input("How much money do you spend on groceries in a week? "))
+ 
+weekly = groceries + times_in_cafeteria * price_of_lunch
+ 
+print("Average food expenditure:")
+print(f"Daily: {weekly / 7} euros")
+print(f"Weekly: {weekly} euros")
+
+
+# How many times a week do you eat at the student cafeteria? 4
+# The price of a typical student lunch? 2.5
+# How much money do you spend on groceries in a week? 28.5
+# 
+# Average food expenditure:
+# Daily: 5.5 euros
+# Weekly: 38.5 euros
