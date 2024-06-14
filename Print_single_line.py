6e9bdb62b105319b2ce41f92bce04f7938608440
laubsj <johndoe@example.comjlaubscher75@gmail.com>
2024-06-14 12:29:31 +1000

    Init

---
+++ Print_single_line.py
@@ -0,0 +1,13 @@
+
+print(5, end= "")
+print(" + ", end= "")
+print(8, end= "")
+print(" - ", end= "")
+print(4, end= "")
+print(" = ", end= "")
+print(5 + 8 - 4)
+
+# 5 + 8 - 4 = 9
+
+# Each print command usually prints out a line of its own, complete with a change of line at the end. 
+# However, if the print command is given an additional argument end = "", it will not print a line change.
