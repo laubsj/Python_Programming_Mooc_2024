f932ab76ee70a79ce7b12444fb1978f12d304966
laubsj <johndoe@example.comjlaubscher75@gmail.com>
2024-06-14 12:41:48 +1000

    init

---
+++ Name_and_Age.py
@@ -0,0 +1,8 @@
+name = input("What is your name: ")
+born = int(input("Which year were you born? "))
+age = 2021 - born
+print(f"Hi {name}, you will be {age} years old at the end of the year 2021")
+
+# What is your name? Frances Fictitious
+# Which year were you born? 1990
+# Hi Frances Fictitious, you will be 31 years old at the end of the year 2021
---
+++ Times_Five.py
@@ -0,0 +1,6 @@
+number = input("Please type in a number: ")
+result = int(number) * 5
+print(f"{number} times 5 is {result}")
+
+# Please type in a number: 3
+# 3 times 5 is 15
