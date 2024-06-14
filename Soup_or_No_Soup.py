68caea8d9b58e2773357cb57d34302e642cccc95
laubsj <johndoe@example.comjlaubscher75@gmail.com>
2024-06-14 15:32:00 +1000

    Init

---
+++ Soup_or_No_Soup.py
@@ -0,0 +1,14 @@
+name = input("Please tell me your name:  ")
+if name != "Jerry":
+    portions = int(input("How many portions of soup? "))
+    cost = 5.9 * portions
+    print("The total cost is", cost)
+print("Next please!")
+
+# Please tell me your name: Kramer
+# How many portions of soup? 2
+# The total cost is 11.8
+# Next please!
+
+# Please tell me your name: Jerry
+# Next please!
