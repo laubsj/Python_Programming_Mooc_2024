3d9948d47686cbf4461a157a41a08934d7f3464e
laubsj <johndoe@example.comjlaubscher75@gmail.com>
2024-06-14 12:50:18 +1000

    init

---
+++ Seconds_in_a_day.py
@@ -0,0 +1,8 @@
+# Write your solution here
+seconds = int(input("How many days?")) * 24 * 60 ** 2
+print(f"Seconds in that many days: {seconds}")
+
+
+
+# How many days? 7
+# Seconds in that many days: 604800
