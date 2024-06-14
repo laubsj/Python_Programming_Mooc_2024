51680b3a3f0c0bdf7de9e8090bbb548d0d2fdc9b
laubsj <johndoe@example.comjlaubscher75@gmail.com>
2024-06-14 12:21:02 +1000

    init

---
+++ Arithmetics.py
@@ -0,0 +1,13 @@
+# Write your solution here
+x = 27
+y = 15
+ 
+print(f"{x} + {y} = {x+y}")
+print(f"{x} - {y} = {x-y}")
+print(f"{x} * {y} = {x*y}")
+print(f"{x} / {y} = {x/y}")
+
+# 27 + 15 = 42
+# 27 - 15 = 12
+# 27 * 15 = 405
+# 27 / 15 = 1.8
