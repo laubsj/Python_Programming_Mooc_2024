9b626f893822bea1f52b2f7068a357c0b753dc0f
laubsj <johndoe@example.comjlaubscher75@gmail.com>
2024-07-02 11:25:06 +1000

    Init

---
+++ number_of_characters.py
@@ -0,0 +1,14 @@
+# My solution
+word = str(input("Please type in a word:"))
+length = len(word)
+if length > 1:
+    print (f"There are {length} letters in the word {word}")
+print("Thank you!")
+# Model Solution
+#word = input("Please type in a word: ")
+# 
+#length = len(word)
+#if length > 1:
+#    print("There are", length, "letters in the word " + word)
+# 
+#print("Thank you!")
