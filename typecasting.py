fc3907a7bf1c8dc022316897b4367600bf656e68
laubsj <johndoe@example.comjlaubscher75@gmail.com>
2024-07-02 11:31:55 +1000

    Init

---
+++ typecasting.py
@@ -0,0 +1,14 @@
+# My solution
+# Write your solution here
+number = float(input("please type in a number:"))
+integer = int(number)
+decimal = number - integer
+
+print(f"Integer part: {integer}")
+print(f"Decimal part: {decimal}")
+#model solution
+#number = float(input("Please type in a number: "))
+#intpart = int(number)
+#decpart = number - intpart
+#print(f"Integer part: {intpart}")
+#print(f"Decimal part: {decpart}")
